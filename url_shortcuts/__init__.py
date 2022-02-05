9187a921b56a891cf6d2bfd71478328ce0517418
makaroshaa <as-makar@ya.ru>
2022-02-05 18:17:24 +0400

    add redirect_ulr route

--- url_shortcuts/__init__.py
+++ url_shortcuts/__init__.py
@@ -1,10 +1,11 @@
 __version__ = '0.1.0'
 
 
-from flask import Flask
+from flask import Flask, url_for
 from flask import render_template
 from flask import request
 from flask import flash
+from flask import redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import uuid
@@ -35,15 +36,26 @@ def index():
             flash('URL must be filled')
         else:
             shortcut_id = generate_shortcut_id()
-            print('shortcut_id ', shortcut_id)
+            print('shortcut_id ', shortcut_id) # debug
             new_item = Shortcuts(url=url, shortcut_id=shortcut_id)
             db.session.add(new_item)
             db.session.commit()
             shortcut_url = request.host_url + shortcut_id
-            print('shortcut_url ', shortcut_url)
+            print('shortcut_url ', shortcut_url) # debug
             flash('Shortcut created')
 
             return render_template('index.html', shortcut_url=shortcut_url)
 
     return render_template('index.html')
 
+@app.route('/<shortcut_id>')
+def redirect_url(shortcut_id):
+    shortcut = Shortcuts.query.filter_by(shortcut_id=shortcut_id).first()
+    print(shortcut) # debug
+
+    if shortcut:
+        return redirect(shortcut.url)
+        
+    else:
+        flash('Invalid link')
+        return redirect(url_for('index'))
