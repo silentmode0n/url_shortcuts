83a98ff004966a1742473092b09d2546d6d88e65
silentmode0n <as.makar@ya.ru>
2022-02-20 19:37:05 +0400

    version 0.5.1: added a table to display a list of shortcuts

--- url_shortcuts/__init__.py
+++ url_shortcuts/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 __author__ = 'silentmode0n'
 
 
@@ -112,6 +112,12 @@ def get_qr_file_buffer(data):
     qr.make_image().save(buf, format='JPEG')
     buf.seek(0)
     return buf
+
+
+@app.template_filter('datetime')
+def _jinja2_filter_datetime(date, format=None):
+    format = format if format else '%d.%m.%Y %H:%M:%S'
+    return date.strftime(format)
 
 
 @app.before_request
