46bd1ac9e0ccb53b07294574af40ae3c715f7946
silentmode0n <as.makar@ya.ru>
2022-05-24 00:07:00 +0400

    changed from sqlite to mysql db for debug mode

--- config.py
+++ config.py
@@ -5,10 +5,16 @@ import uuid
 SECRET_KEY = os.environ.get('SECRET_KEY') or uuid.uuid4().hex
 CSRF_ENABLED = True
 
-DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///shortcuts.db' #sqlite for debug from localhost
-if DATABASE_URI.startswith("postgres://"):
-    DATABASE_URI = DATABASE_URI.replace("postgres://", "postgresql://", 1) # for deploy on heroku with postgres db
+#sqlite for debug from localhost
+# DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///shortcuts.db' 
+#mysql for debug from localhost
+DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+mysqlconnector://user:userpassword@localhost/shortdb'
 
+# for deploy on heroku with postgres db
+if DATABASE_URI.startswith("postgres://"):
+    DATABASE_URI = DATABASE_URI.replace("postgres://", "postgresql://", 1) 
+
+# config for SqlAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENGINE_OPTIONS = {"pool_recycle": 299}
